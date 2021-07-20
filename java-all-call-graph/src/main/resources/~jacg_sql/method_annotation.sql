48163b409514cb3e1b42cc28f628d1c8f8dcf569
Adrninistrator <Adrninistrator>
2021-07-20 21:39:46 +0800

    make the database field length larger

--- java-all-call-graph/src/main/resources/~jacg_sql/method_annotation.sql
+++ java-all-call-graph/src/main/resources/~jacg_sql/method_annotation.sql
@@ -1,6 +1,6 @@
 CREATE TABLE if not exists method_annotation_{appName} (
   method_hash varchar(30) NOT NULL COMMENT '方法hash+字节数',
-  annotation_name varchar(100) NOT NULL COMMENT '注解名',
+  annotation_name varchar(150) NOT NULL COMMENT '注解名',
   full_method varchar(767) NOT NULL COMMENT '完整方法（类名+方法名+参数）',
   PRIMARY KEY (method_hash, annotation_name),
   INDEX idx_ma_annotation_name(annotation_name)
